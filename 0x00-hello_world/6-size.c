71b7c0a1a6901fe6af1f54f0f5406fe6928d9dce
ultron <mulerfrommarss@gmail.com>
2023-02-16 16:19:41 +0300

    Betty lint error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int main(void) {
-	char charVar;
-	int intVar;
-	long int longIntVar;
-	long long int longLongIntVar;
-	float floatVar;
-
-	printf("Size of a char: %d byte(s)\n", sizeof(charVar));
-	printf("Size of an int: %d byte(s)\n", sizeof(intVar));
-	printf("Size of a long int: %d byte(s)\n", sizeof(longIntVar));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(longLongIntVar));
-	printf("Size if a float: %d bytes(s)\n", sizeof(floatVar));
-
-	return(0);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
