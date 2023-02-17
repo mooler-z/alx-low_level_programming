e7de4f41f43d89f3d4a6a4cf7429a1e59e6843e0
ultron <mulerfrommarss@gmail.com>
2023-02-17 15:02:25 +0300

    lowercase upto uppercase alphabets[fixed bug]

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description:  prints the alphabet in lowercase,
- * and then in uppercase, followed by a new line.
  * Return: 0
  */
 int main(void)
