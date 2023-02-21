a1dc8870b3a0eb774bc9b3e556c8ed40f562a2c4
ultron <mulerfrommarss@gmail.com>
2023-02-21 14:54:12 +0300

    _putchar lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: void
+ * Author: Mulugeta Z
+ *
+ * Description: simply print lowercase alphabets
+ * Created:  Tue Feb 21 02:50:06 PM EAT 2023
+ * Filename: 1-alphabet.c
+ */
+void main(void)
+{
+	char c = 97;
+
+	while (c <= 122)
+	{
+		_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
+
