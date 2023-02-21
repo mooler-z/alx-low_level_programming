ba8b94cc815e1e2269d7f68576fa2b0fbb81dd89
ultron <mulerfrommarss@gmail.com>
2023-02-21 15:48:20 +0300

    24 (series, lol)

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry
+ * Return: 0 (on success)
+ * Author: Mulugeta Z
+ *
+ * Description: print 24 hours
+ * Created:  Tue Feb 21 03:43:37 PM EAT 2023
+ * Filename: 8-24_hours.c
+ */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	hr = 0;
+	for (; hr < 24;)
+	{
+		min = 0;
+		for (; min < 60;)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+		min++;
+	}
+	hr++;
+}
+
