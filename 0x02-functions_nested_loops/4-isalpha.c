142d890b506360452d30f69557b1e06b0d667871
ultron <mulerfrommarss@gmail.com>
2023-02-21 15:16:46 +0300

    check if its alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry
+ * Return: 1 if alpha else 0
+ * @c: letter to be checked
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Tue Feb 21 03:11:49 PM EAT 2023
+ * Filename: 4-isalpha.c
+ */
+int _isalpha(int c)
+{
+	if ((c <= 122 && c >=97) || (c <= 90 && c >= 65))
+		return (0);
+	else
+		return (1);
+}
+
