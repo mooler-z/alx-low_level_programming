6c61c5a895548c850efb8ddc843f38c79d2554ef
ultron <mulerfrommarss@gmail.com>
2023-02-21 16:15:11 +0300

    tricky when printing

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/* #include "main.h" */
+
+/**
+ * times_table - Entry
+ * Return: void
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Tue Feb 21 03:51:46 PM EAT 2023
+ * Filename: 9-times_table.c
+ */
+void times_table(void)
+{
+	int col, row;
+
+	col = 0;
+	for (; col < 10;)
+	{
+		row = 0;
+		for (; row < 10;)
+		{
+			/* printf("%d",(col * row)); */
+			if ((col * row) < 10)
+			{
+				_putchar((col * row) + '0');
+			}
+			else
+			{
+				_putchar(((col * row) / 10) + '0');
+				_putchar(((col * row) % 10) + '0');
+
+			}
+			if (row < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			
+			if ((col * row) < 10)
+			{
+				_putchar(' ');
+			}
+			row++;
+		}
+		col++;
+		_putchar('\n');
+	}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -24,3 +24,4 @@ int print_sign(int n);
 int _abs(int j);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
