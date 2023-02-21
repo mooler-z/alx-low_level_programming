f5bbaab993521e09ed67f2de533f818ad61bf0b0
ultron <mulerfrommarss@gmail.com>
2023-02-21 16:54:13 +0300

    sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry
+ * Return: sum of @x and @y
+ * @x: first number
+ * @y: second number
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Tue Feb 21 04:51:46 PM EAT 2023
+ * Filename: 10-add.c
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
