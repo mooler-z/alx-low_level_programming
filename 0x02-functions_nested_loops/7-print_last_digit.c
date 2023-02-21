59649895d51d607eedb8d732cff59c1dca76718d
ultron <mulerfrommarss@gmail.com>
2023-02-21 15:37:02 +0300

    last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry
+ * Return: last digit of the number
+ * @n: number from which the last digi is going to be extracted
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Tue Feb 21 03:33:30 PM EAT 2023
+ * Filename: 7-print_last_digit.c
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n = -1 * n;
+
+	_putchar(n % 10);
+	return (n % 10);
+}
+
