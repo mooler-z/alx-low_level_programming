59697e3e3eb37cc7cda94b328ed9184b72512f58
ultron <mulerfrommarss@gmail.com>
2023-02-21 18:09:41 +0300

    even fabs

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that finds and prints the sum of the even-valued terms
+ * Return: 0 (on success)
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Tue Feb 21 06:08:13 PM EAT 2023
+ * Filename: 103-fibonacci.c
+ */
+int main(void)
+{
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
+
