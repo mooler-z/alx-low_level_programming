34ae916dcf9e696d6648e6916a071c500530b793
ultron <mulerfrommarss@gmail.com>
2023-02-22 14:06:52 +0300

    test postive negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: 0 (on success)
+ * Author: Mulugeta Z
+ *
+ * Description: test postive_negative
+ * Created:  Wed Feb 22 02:03:02 PM EAT 2023
+ * Filename: 0-main.c
+ */
+int main(void)
+{
+	int n;
+
+	n = 42;
+	positive_or_negative(n);
+
+	return (0);
+}
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/22/2023 02:05:10 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mulugeta Z (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+void positive_or_negative(int i);
