62b83ebf1770566528dbe459e7a7f070d16290ce
ultron <mulerfrommarss@gmail.com>
2023-02-22 14:39:34 +0300

    minor bug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,20 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  main.h
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  02/22/2023 02:05:10 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Mulugeta Z (), 
- *   Organization:  
- *
- * =====================================================================================
- */
 #ifndef MAIN_H
 #define MAIN_H
 
