b253771566d24fa43d62304de555dfaf5a0aede1
ultron <mulerfrommarss@gmail.com>
2023-02-23 23:48:46 +0300

    product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - product of two integers
+ * @a: first int
+ * @b: second int
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
