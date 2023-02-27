5a1a3c0823fc98d8381f9ae5935d1d944d07c68d
ultron <mulerfrommarss@gmail.com>
2023-02-27 11:34:53 +0300

    ReadMe and then reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry
+ * @n: pointer to the variable to be updated
+ * Return: void
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Mon Feb 27 11:30:01 AM EAT 2023
+ * Filename: 0-reset_to_98.c
+ */
+int reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(int c);
+void reset_to_98(int *n);
+
+#endif
