2f71627b26bf1bb3b03b760c1df459a3c3f07175
ultron <mulerfrommarss@gmail.com>
2023-02-27 15:08:39 +0300

    halvzzzieess

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry
+ * @str: string to be halved
+ * Return: void
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Mon Feb 27 03:02:56 PM EAT 2023
+ * Filename: 7-puts_half.c
+ */
+void puts_half(char *str)
+{
+	int len, counter, evenOdd;
+
+	len = 0;
+	for (; str[len] != '\0'; len++)
+	{}
+
+	if (len % 2 == 1)
+		evenOdd = (len - 1) / 2;
+	else
+		evenOdd = len / 2;
+
+	for (counter = evenOdd; counter < len; counter++)
+		_putchar(str[counter]);
+
+	_putchar('\n');
+}
+
