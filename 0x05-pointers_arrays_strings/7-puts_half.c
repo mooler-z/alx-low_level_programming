4fda0c9b043e590752f571b33f36abe7b1b04027
ultron <mulerfrommarss@gmail.com>
2023-02-27 15:14:17 +0300

    odd error

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@
  */
 void puts_half(char *str)
 {
-	int len, counter, evenOdd;
+	int len, counter, evenOdd, cond;
 
 	len = 0;
 	for (; str[len] != '\0'; len++)
@@ -20,14 +20,22 @@ void puts_half(char *str)
 
 	if (len % 2 == 1)
 	{
-		len = len - 1;
+		cond = len - 1;
 		evenOdd = (len - 1) / 2;
 	}
 	else
+	{
+		cond = len;
 		evenOdd = len / 2;
+	}
 
-	for (counter = evenOdd; counter < len; counter++)
-		_putchar(str[counter]);
+	for (counter = evenOdd; counter < cond; counter++)
+	{
+		if (len % 2 == 0)
+			_putchar(str[counter]);
+		else
+			_putchar(str[counter + 1]);
+	}
 
 	_putchar('\n');
 }
