31ae9b4c23b1f9ad07dcc80171c4e7342a8f4535
ultron <mulerfrommarss@gmail.com>
2023-02-27 14:23:18 +0300

    reverse a given string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry
+ * @s: string to be reversed
+ * Return: void
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Mon Feb 27 02:17:43 PM EAT 2023
+ * Filename: 5-rev_string.c
+ */
+void rev_string(int *s)
+{
+	int looper, counter, reverser;
+	char tempCharacter;
+
+	counter = 0;
+	for (; s[counter] != '\0'; counter++)
+	{}
+
+	reverser = counter - 1;
+	for (looper = 0; looper < (counter / 2); looper++)
+	{
+		tempCharacter = s[looper];
+		s[looper] = s[reverser];
+		s[reverser] = tempCharacter;
+		reverser--;
+	}
+}
+
