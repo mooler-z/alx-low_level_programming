4604d98bfc4f99ea4c160ba1681f9c4db618ba92
ultron <mulerfrommarss@gmail.com>
2023-02-27 15:21:21 +0300

    print numbers from an int array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - Entry
+ * @a: pointer to the array
+ * @n: length of array
+ * Return: void
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Mon Feb 27 03:15:15 PM EAT 2023
+ * Filename: 8-print_array.c
+ */
+void print_array(int *a, int n)
+{
+	int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		if (counter == 0)
+			printf("%d", a[counter]);
+		else
+			printf(", %d", a[counter]);
+	}
+}
+
