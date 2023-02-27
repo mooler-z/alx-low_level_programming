7801178e3bd65a01de1623fdf41b61f8f0ca18db
ultron <mulerfrommarss@gmail.com>
2023-02-27 15:28:47 +0300

    copy and paste

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcpy - Entry
+ * Return: char
+ * @src: source of string
+ * @dest: where to be copied to
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Mon Feb 27 03:24:27 PM EAT 2023
+ * Filename: 9-strcpy.c
+ */
+char *_strcpy(char *src, char *dest)
+{
+	int counter, len;
+
+	len = 0;
+	for (; src[len] != '\0'; len++)
+	{}
+
+	for (counter = 0; counter < len; counter++)
+		dest[counter] = src[counter];
+
+	dest[counter] = '\0'
+	return (dest);
+}
+
