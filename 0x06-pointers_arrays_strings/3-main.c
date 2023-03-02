f65f772a9af49a1c1d621ac69e27dbd6373d1648
ultron <mulerfrommarss@gmail.com>
2023-03-02 16:51:26 +0300

    'string' < 'comparison'

---
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  3-main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/02/2023 04:49:31 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
+}
