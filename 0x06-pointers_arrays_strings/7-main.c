79a2a8e4be2b2ce32aaf4eb0b122eb6b6b741867
ultron <mulerfrommarss@gmail.com>
2023-03-02 18:32:12 +0300

    🔥 string

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  7-main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/02/2023 06:03:03 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
+
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
+}
