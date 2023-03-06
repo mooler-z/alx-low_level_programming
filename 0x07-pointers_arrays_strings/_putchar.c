b3c499f1f5cf84d6594a209b32590c6515ecb9d6
ultron <mulerfrommarss@gmail.com>
2023-03-06 15:20:48 +0300

    accept char as arg

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Entry
+ * @c: character to be printed
+ * Return: 1 (on success) else -1
+ * Author: Mulugeta Z
+ *
+ * Description:
+ * Created:  Mon Feb 27 11:24:33 AM EAT 2023
+ * Filename: _putchar.c
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
